a6044b40f1583dbd550662a9304121a30d1056da
Laura <laurao148@gmail.com>
2018-07-23 00:38:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Upper West Side';
 mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
-companyCeo = 'Lauren Hart';
+
